57c550c55463d94395d794ef2395dda5c836d2c3
Jose Mari Ronquillo <ronquillo.236225@pasay-edsa.sti.edu.ph>
2022-11-22 00:59:25 +0800

    Updated

--- GagoZoo/Properties/Settings.Designer.cs
+++ GagoZoo/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace GagoZoo.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -42,6 +42,17 @@ namespace GagoZoo.Properties {
         public string KalayaanZooConnectionString {
             get {
                 return ((string)(this["KalayaanZooConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=ROJIE,1433;Initial Catalog=KalayaanZoo;User ID=sa;Password=sysadmin12" +
+            "345")]
+        public string KalayaanZooConnectionString1 {
+            get {
+                return ((string)(this["KalayaanZooConnectionString1"]));
             }
         }
     }
